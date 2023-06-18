4f87f1f617c1b260f80cbc111b81ac62e9e02a6a
sankhadipDey <86321435+sankhadipDey@users.noreply.github.com>
2023-06-18 14:15:08 +0530

    Update gcstohdfs.py
    loading of appearances table

--- src/gcstohdfs.py
+++ src/gcstohdfs.py
@@ -1,1 +1,20 @@
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder \
+    .appName("GCS to HDFS Transfer") \
+    .getOrCreate()
+
+# Set the GCS and HDFS paths
+gcs_path = "gs://transfermarket/sourcedata/appearances.csv"
+hdfs_path = "/user/hadoop/trasansfermarket/"
+
+# Read the table from GCS
+df = spark.read.format("csv").option("header", "true").load(gcs_path)
+
+# Write the table to HDFS
+df.write.mode("overwrite").format("parquet").saveAsTable(transfermarket_analysis.appearances)
+
+# Stop the SparkSession
+spark.stop()
 
