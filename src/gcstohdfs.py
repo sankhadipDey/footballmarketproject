edb184b0cadd7877f0c5e30e0202cddf8c829217
sankhadipDey <86321435+sankhadipDey@users.noreply.github.com>
2023-06-22 01:55:08 +0530

    Update gcstohdfs.py
    added using OOPs concept

--- src/gcstohdfs.py
+++ src/gcstohdfs.py
@@ -1,20 +1,44 @@
 from pyspark.sql import SparkSession
+import sys
 
-# Create SparkSession
-spark = SparkSession.builder \
-    .appName("GCS to HDFS Transfer") \
-    .getOrCreate()
+class GcsToHdfs:
+    def __init__(self, gcs_path, hdfs_path):
+        self.gcs_path = gcs_path
+        self.hdfs_path = hdfs_path
+        self.spark = SparkSession.builder \
+            .appName("CSV to Parquet Conversion") \
+            .getOrCreate()
 
-# Set the GCS and HDFS paths
-gcs_path = "gs://transfermarket/sourcedata/appearances.csv"
-hdfs_path = "/user/hadoop/trasansfermarket/"
+    def convert(self):
+        try:
+            df = self.spark.read.format("csv") \
+                .option("header", "true") \
+                .load(self.gcs_path)
 
-# Read the table from GCS
-df = spark.read.format("csv").option("header", "true").load(gcs_path)
+            df.write.mode("overwrite") \
+                .parquet(self.hdfs_path)
+				.option("compression","snappy")
+            print("Conversion completed successfully.")
+        except Exception as e:
+            print("Error during conversion:", str(e))
+        finally:
+            self.spark.stop()
 
-# Write the table to HDFS
-df.write.option("compression","snappy").mode("overwrite").format("parquet").saveAsTable('transfermarket_analysis.appearances',path=hdfs_path)
+if __name__ == "__main__":
+    # Get the CSV file name and HDFS output path from command-line arguments
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <csv_file_name> <hdfs_output_path>")
+        sys.exit(1)
 
-# Stop the SparkSession
-spark.stop()
+    csv_file_name = sys.argv[1]
+    hdfs_output_path = sys.argv[2]
 
+    # Set GCS and HDFS paths
+    gcs_path = f"gs://your-gcs-bucket/path/to/csv/{csv_file_name}"
+    hdfs_path = f"hdfs:///path/to/parquet/output/{csv_file_name}.parquet"
+
+    # Create an instance of GcsToHdfs
+    converter = GcsToHdfs(gcs_path, hdfs_path)
+
+    # Convert CSV to Parquet
+    converter.convert()
