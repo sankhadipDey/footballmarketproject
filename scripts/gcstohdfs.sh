7c2347ff6dbebc5f70358926b378b1b12216d0c9
sankhadipDey <86321435+sankhadipDey@users.noreply.github.com>
2023-06-29 01:51:38 +0530

    Update gcstohdfs.sh

--- scripts/gcstohdfs.sh
+++ scripts/gcstohdfs.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+# Set the necessary configurations
+CLUSTER_NAME="<cluster-name>"
+REGION="<region>"
+BUCKET_NAME="<your-bucket>"
+SCRIPT_PATH="gs://$BUCKET_NAME/path/to/gcstohdfs.py"
+JARS_PATH="gs://$BUCKET_NAME/path/to/gcs-connector.jar"
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./gcstohdfs.sh <csv_file_name> <hdfs_output_path>"
+    exit 1
+fi
+
+# Extract the arguments
+CSV_FILE_NAME=$1
+HDFS_OUTPUT_PATH=$2
+
+# Set the GCS and HDFS paths
+GCS_PATH="gs://$BUCKET_NAME/path/to/csv/$CSV_FILE_NAME"
+HDFS_PATH="hdfs:///path/to/parquet/output/${CSV_FILE_NAME}.parquet"
+
+# Build the spark-submit command
+SPARK_SUBMIT_CMD="gcloud dataproc jobs submit pyspark"
+SPARK_SUBMIT_CMD+=" --cluster $CLUSTER_NAME"
+SPARK_SUBMIT_CMD+=" --region $REGION"
+SPARK_SUBMIT_CMD+=" --jars $JARS_PATH"
+SPARK_SUBMIT_CMD+=" -- $SCRIPT_PATH $CSV_FILE_NAME $HDFS_OUTPUT_PATH"
+
+# Execute the spark-submit command
+eval "$SPARK_SUBMIT_CMD"
